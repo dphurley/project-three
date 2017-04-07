ada062fb007ad864e4d0f3c6bb6e46dc3c4c61b8
Danny Hurley <hurley.dannyp@gmail.com>
2017-04-07 14:35:23 -0400

    Installed Mongoose and wired up local database.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:project_three', function () {
+    console.log('Mongo DB connected!');
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("App listening successfully");
+    console.log('App listening successfully');
 });
